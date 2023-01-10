a132ab30d76d085758ac614977e393720f390c25
runesc <luisalfredoreyes98@gmail.com>
2023-01-10 15:40:45 -0600

    build 1.0.1

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ def open_file(fname):
 setup(
   name = 'word2number_es',
   packages = ['word2number_es'],  # this must be the same as the name above
-  version = '1.0',
+  version = '1.0.0',
   license=open('LICENSE.txt').read(),
-  description = 'Convert number words (eg. twenty one) to numeric digits (spanish)',
+  description = 'Convert number words (eg. veintiuno) to numeric digits (spanish)',
   author = 'Neuri',
   author_email = 'support@neuri.ai',
   url = 'https://github.com/Neuri-ai/w2n_es',  # use the URL to the github repo
@@ -23,5 +23,6 @@ setup(
       'Intended Audience :: Developers',
       'Programming Language :: Python'
   ],
-  long_description=open_file('README.rst').read()
+  long_description=open_file('README.rst').read(),
+  python_requires = ">=3.6"
 )
